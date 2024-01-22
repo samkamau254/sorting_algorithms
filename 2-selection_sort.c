4aa1c028ccd1847d7b5bbb276c2e87f5f2c7c9c3
samkamau254 <samkamau254@outlook.com>
2024-01-22 06:56:51 -0500

    arrays sorted

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * selection_sort - The function sorts the integers in ascending order.
+ * @array: Array to be sorted
+ * @size: Size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, j, min;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+			{
+				min = j;
+			}
+		}
+
+		if (i != min)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
+}
